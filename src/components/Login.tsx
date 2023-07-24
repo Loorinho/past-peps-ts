a68780340eb172ff4c2c66be703412b457f05309
Jacobson <romeojacobson@gmail.com>
2023-07-24 17:26:39 +0300

    Added login page content

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,117 @@
+import { useFormik } from "formik";
+import { Link } from "react-router-dom";
+import * as yup from "yup";
+import axios from "axios";
+import LoginInput from "./utilities/LoginInput";
+
+type LoginDetails = {
+  username: string;
+  password: string;
+};
+
 const Login = () => {
-  return <div>Login page</div>;
+  const initialValues: LoginDetails = {
+    username: "",
+    password: "",
+  };
+
+  const LoginSchema = yup.object().shape({
+    username: yup.string().required().label("Username"),
+    password: yup.string().required().label("Password"),
+  });
+
+  const {
+    handleBlur,
+    handleChange,
+    handleSubmit,
+    errors,
+    touched,
+    isSubmitting,
+  } = useFormik({
+    initialValues: initialValues,
+    onSubmit: async (values) => {
+      const url = "http://127.0.0.1:8000/api/login";
+      console.log("Values: ", values);
+    },
+    validationSchema: LoginSchema,
+  });
+
+  return (
+    <>
+      <div className="login_form border-2 border-green-700 bg-white rounded-lg p-5 mx-auto">
+        <header className="flex justify-center items-center flex-col">
+          {/* <img
+        //    src={MukLogo}
+           alt="logo"
+           style={{ width: "200px", height: "200px" }}
+           className="me-5 muk_logo"
+           loading="lazy"
+         /> */}
+          <p className="text-green-700 font-semibold text-xl">Peps</p>
+          {/* <p className="text-green-700 font-semibold">Teaching Load</p> */}
+        </header>
+        <div className="">
+          <div className=" bg-white mt-2 rounded-md mb-5">
+            <form className="" onSubmit={handleSubmit}>
+              <h4 className="text-green-700 text-center text-xl font-semibold mb-5">
+                Login into your account
+              </h4>
+              <div>
+                <LoginInput
+                  label="Email"
+                  name="username"
+                  type="text"
+                  touched={touched}
+                  errors={errors}
+                  placeholder="Please enter your username here..."
+                  handleChange={handleChange}
+                  handleBlur={handleBlur}
+                />
+              </div>
+              <div>
+                <LoginInput
+                  label="Password"
+                  name="password"
+                  type="password"
+                  touched={touched}
+                  errors={errors}
+                  placeholder="Please enter your password name.."
+                  handleChange={handleChange}
+                  handleBlur={handleBlur}
+                />
+              </div>
+
+              <div className="flex justify-center items-center w-full">
+                {isSubmitting ? (
+                  <button
+                    className="w-full text-white px-4 rounded py-2 bg-green-400 mt-2 "
+                    type="submit"
+                    disabled
+                  >
+                    Login
+                  </button>
+                ) : (
+                  <button
+                    className="w-full text-white px-4 rounded py-2 bg-green-700 mt-2 hover:scale-95"
+                    type="submit"
+                  >
+                    Login
+                  </button>
+                )}
+              </div>
+            </form>
+            <p className="mt-3 text-dark">
+              Don't have an account?{" "}
+              <Link to="/register">
+                <span className="text-green-700">Register</span>
+              </Link>{" "}
+              here
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
---
+++ src/components/utilities/LoginInput.tsx
@@ -0,0 +1,58 @@
+import { FormikTouched, FormikErrors } from "formik";
+
+type initialValues = {
+  [key: string]: any;
+  username: string;
+  password: string;
+};
+type LoginInputPops = {
+  label: string;
+  name: string;
+  type: string;
+  placeholder?: string;
+  errors: FormikErrors<initialValues>;
+  handleChange: (e: React.ChangeEvent<any>) => void;
+  handleBlur: (e: React.FocusEvent<any, Element>) => void;
+  touched: FormikTouched<initialValues>;
+};
+
+const LoginInput = ({
+  label,
+  name,
+  type,
+  placeholder,
+  handleChange,
+  handleBlur,
+  touched,
+  errors,
+}: LoginInputPops) => {
+  // const myname = name;
+  return (
+    <div>
+      <label
+        htmlFor={name}
+        className="block font-normal text-sm mt-2 text-gray-700"
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        className={`${
+          errors[`${name}`] && touched[`${name}`]
+            ? "focus:ring-1 focus:ring-red-500 border-red-500 focus:border-red-500"
+            : "focus:ring-1 focus:ring-green-700 focus:border-teal-500"
+        } focus:outline-none shadow-sm py-2 px-4 w-full rounded`}
+        autoComplete="off"
+        placeholder={placeholder}
+        onChange={handleChange}
+        onBlur={handleBlur}
+      />
+      {errors[`${name}`] && touched[`${name}`] && (
+        <span className="block text-red-500">{errors[name]} </span>
+      )}
+    </div>
+  );
+};
+
+export default LoginInput;
